88f19370753100f97c6bf187d2813fea6bdd8673
Santiago <santiago@Santiagos-MacBook-Pro.local>
2025-04-27 23:51:04 -0300

    Added landing and landing headers created

--- src/frontend/main.jsx
+++ src/frontend/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import App from './components/App.jsx'
+import LandingPage from './pages/landing-page.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <LandingPage />
   </StrictMode>,
 )
---
+++ src/frontend/pages/landing-page.jsx
@@ -0,0 +1,19 @@
+import '../styles/landing-page.css'
+
+function LandingPage() {
+  return (
+    <div className="landing">
+      <div className="header">
+        <div className="left-side">
+        <a href=''>Product</a>
+        <a href=''>Features</a>
+        <a href=''>Pricing</a>
+        <a href=''>Contact</a>
+        <a href=''>About</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
